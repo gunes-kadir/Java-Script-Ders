e3a59cbaa291d2f81e2ee2aa5b88c1f780849b53
gunes-kadir <kadirgunes.work@gmail.com>
2024-11-13 09:54:13 +0300

    degiskenler uygulaması yapıldı

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("sayfa yüklendi");
+var ad = prompt ("Adınızı Giriniz : ");
+var ogrno = prompt (" Öğrenci no : ");
+var cinsiyet = prompt("cinsiyet : "); // erkek : true - kadın : false
+var ders = prompt ("Ders adı : ");
+var not1 = prompt ("Not 1 : ");
+var not2 = prompt ("Not 2 :");
+var ortalama = (Number(not1) + Number(not2))/2;
+
+
+
+
+
+console.log(ogrno + " numaralı " + ad +" ("+cinsiyet+") " +   "isimli öğrencinin " + ders + 
+    " dersinden aldığı ortalama notu " + ortalama );
